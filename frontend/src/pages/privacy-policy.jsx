ea78d0d7ed3a11b9f2f52758d534b920049ec61b
Patelshubh111 <120030453+Patelshubh111@users.noreply.github.com>
2024-09-10 04:46:16 +0530

    Create privacy-policy.jsx

---
+++ frontend/src/pages/privacy-policy.jsx
@@ -0,0 +1,14 @@
+import dynamic from "next/dynamic";
+import Seo from "../components/common/seo";
+import PrivacyPolicy from "../components/privacy-policy";
+
+const index = () => {
+  return (
+    <>
+      <Seo pageTitle="Privacy Policy" />
+      <PrivacyPolicy />
+    </>
+  );
+};
+
+export default dynamic(() => Promise.resolve(index), { ssr: false });
